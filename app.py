a11b86c0d57d722c0f0fdb17de265c27ab08a558
oliviacolin <oliviacolin@developmentontheedge.com>
2020-01-28 09:10:24 +0700

    Commit from PyCharm 1

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', title=data.title, subtitle=data.subtitle, description=data.description, departures=data.departures, tours=data.tours)
 
 
 @app.route('/from/')
