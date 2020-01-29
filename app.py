5d1ec548f36f488af6ac98123a810231f6cb44e9
oliviacolin <oliviacolin@developmentontheedge.com>
2020-01-29 16:09:11 +0700

    Commit from PyCharm tour project

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
 from flask import Flask, render_template
 import data
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html', title=data.title, subtitle=data.subtitle, description=data.description, departures=data.departures, tours=data.tours)
+    main_page = render_template('index.html', data=data)
+    return main_page
 
 
-@app.route('/from/')
-def direction():
-    return render_template('direction.html')
+@app.route('/from/<direction>/')
+def direction(direction):    
+    dir_from = {}
+    prices = []
+    nights = []
+    for key in data.tours:
+        if data.tours[key]["departure"] == direction:
+            print(data.tours[key]["departure"])
+            dir_from[key] = data.tours[key]
+            prices.append(data.tours[key]["price"])
+            nights.append(data.tours[key]["nights"])
+    direction_page = render_template('direction.html', dir_from=dir_from, prices=prices, nights=nights, direction=direction, data=data)
+    return direction_page
 
 
-@app.route('/tour/')
-def tour():
-    return render_template('tour.html')
+@app.route('/tour/<id>/')
+def tour(id):
+    tour_page = render_template('tour.html', id=id, data=data)
+    return tour_page
 
 
-app.run()
+@app.errorhandler(404)
+def not_found(e):
+    return "С нами сюда не попасть. Попробуйте вернуться на главную и выбрать что-то другое."
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return "Что-то не так, но мы ЭТО уже починим."
+
+
+if __name__ == '__main__':
+    app.run()
+
